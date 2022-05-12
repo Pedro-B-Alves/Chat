6a07ab5eca217ccdf5e8212fadd242cea9c1bf18
Pedro-B-Alves <pedro22carro@gmail.com>
2022-05-12 13:31:29 -0300

    Chat - cliente

---
+++ ChatCliente/FormCliente.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+namespace ChatCliente
+{
+    partial class FormCliente
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormCliente));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtServidorIP = new System.Windows.Forms.TextBox();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.txtMensagem = new System.Windows.Forms.TextBox();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(54, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(91, 79);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(207, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Servidor IP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 17);
+            this.label2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(207, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Usuário";
+            // 
+            // txtServidorIP
+            // 
+            this.txtServidorIP.Location = new System.Drawing.Point(301, 31);
+            this.txtServidorIP.Name = "txtServidorIP";
+            this.txtServidorIP.Size = new System.Drawing.Size(220, 22);
+            this.txtServidorIP.TabIndex = 4;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(301, 73);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(220, 22);
+            this.txtUsuario.TabIndex = 5;
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(551, 70);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(95, 28);
+            this.btnConectar.TabIndex = 6;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(52, 112);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(593, 454);
+            this.txtLog.TabIndex = 7;
+            // 
+            // txtMensagem
+            // 
+            this.txtMensagem.Location = new System.Drawing.Point(54, 587);
+            this.txtMensagem.Name = "txtMensagem";
+            this.txtMensagem.Size = new System.Drawing.Size(467, 22);
+            this.txtMensagem.TabIndex = 8;
+            this.txtMensagem.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtMensagem_KeyPress);
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(545, 584);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(100, 29);
+            this.btnEnviar.TabIndex = 9;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // FormCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleGreen;
+            this.ClientSize = new System.Drawing.Size(700, 625);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.txtMensagem);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnConectar);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.txtServidorIP);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormCliente";
+            this.Text = "Chat - Cliente";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtServidorIP;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.TextBox txtMensagem;
+        private System.Windows.Forms.Button btnEnviar;
+    }
+}
+
---
+++ ChatCliente/FormCliente.cs
@@ -0,0 +1,177 @@
+﻿using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using System;
+
+namespace ChatCliente
+{
+    public partial class FormCliente : Form
+    {
+        // Responsável por trata o nome do usuário
+        private string NomeUsuario = "Desconhecido";
+        private StreamWriter stwEnviador;
+        private StreamReader strReceptor;
+        private TcpClient tcpServidor;
+        // É necessário para atualizar o formulário com mensagens da outra thread
+        private delegate void AtualizaLogCallBack(string strMensagem);
+        // É necessário para definir o formulário para o estado "disconnected" de outra thread
+        private delegate void FechaConexaoCallBack(string strMotivo);
+        private Thread mensagemThread;
+        private IPAddress enderecoIP;
+        private bool Conectado;
+
+        public FormCliente()
+        {
+            // Na saida da aplicação : desconectar
+            Application.ApplicationExit += new EventHandler(OnApplicationExit);
+            InitializeComponent();
+        }
+
+        private void btnConectar_Click(object sender, System.EventArgs e)
+        {
+            // se não esta conectando aguarda a conexão
+            if (Conectado == false)
+            {
+                // Inicializa a conexão
+                InicializaConexao();
+            }
+            else // Se esta conectado entao desconecta
+            {
+                FechaConexao("Desconectado a pedido do usuário.");
+            }
+        }
+        private void InicializaConexao()
+        {
+            try
+            {
+                // Trata o endereço IP informado em um objeto IPAdress
+                enderecoIP = IPAddress.Parse(txtServidorIP.Text);
+                // Inicia uma nova conexão TCP com o servidor chat
+                tcpServidor = new TcpClient();
+                tcpServidor.Connect(enderecoIP, 2502);
+
+                // AJuda a verificar se estamos conectados ou não
+                Conectado = true;
+
+                // Prepara o formulário
+                NomeUsuario = txtUsuario.Text;
+
+                // Desabilita e habilita os campos apropriados
+                txtServidorIP.Enabled = false;
+                txtUsuario.Enabled = false;
+                txtMensagem.Enabled = true;
+                btnEnviar.Enabled = true;
+                btnConectar.Text = "Desconectado";
+
+                // Envia o nome do usuário ao servidor
+                stwEnviador = new StreamWriter(tcpServidor.GetStream());
+                stwEnviador.WriteLine(txtUsuario.Text);
+                stwEnviador.Flush();
+
+                //Inicia a thread para receber mensagens e nova comunicação
+                mensagemThread = new Thread(new ThreadStart(RecebeMensagens));
+                mensagemThread.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro : " + ex.Message, "Erro na conexão com servidor", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void RecebeMensagens()
+        {
+            // recebe a resposta do servidor
+            strReceptor = new StreamReader(tcpServidor.GetStream());
+            string ConResposta = strReceptor.ReadLine();
+            // Se o primeiro caractere da resposta é 1 a conexão foi feita com sucesso
+            if (ConResposta[0] == '1')
+            {
+                // Atualiza o formulário para informar que está conectado
+                this.Invoke(new AtualizaLogCallBack(this.AtualizaLog), new object[] { "Conectado com sucesso!" });
+            }
+            else // Se o primeiro caractere não for 1 a conexão falhou
+            {
+                string Motivo = "Não Conectado: ";
+                // Extrai o motivo da mensagem resposta. O motivo começa no 3º caractere
+                Motivo += ConResposta.Substring(2, ConResposta.Length - 2);
+                // Atualiza o formulário como o motivo da falha na conexão
+                this.Invoke(new FechaConexaoCallBack(this.FechaConexao), new object[] { Motivo });
+                // Sai do método
+                return;
+            }
+
+            // Enquanto estiver conectado le as linhas que estão chegando do servidor
+            while (Conectado)
+            {
+                // exibe mensagens no Textbox
+                this.Invoke(new AtualizaLogCallBack(this.AtualizaLog), new object[] { strReceptor.ReadLine() });
+            }
+        }
+
+        private void AtualizaLog(string strMensagem)
+        {
+            // Anexa texto ao final de cada linha
+            txtLog.AppendText(strMensagem + "\r\n");
+        }
+
+        private void btnEnviar_Click(object sender, System.EventArgs e)
+        {
+            EnviaMensagem();
+        }
+
+        private void txtMensagem_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Se pressionou a tecla Enter
+            if (e.KeyChar == (char)13)
+            {
+                EnviaMensagem();
+            }
+        }
+
+        // Envia a mensagem para o servidor
+        private void EnviaMensagem()
+        {
+            if (txtMensagem.Lines.Length >= 1)
+            { //escreve a mensagem da caixa de texto
+                stwEnviador.WriteLine(txtMensagem.Text);
+                stwEnviador.Flush();
+                txtMensagem.Lines = null;
+            }
+            txtMensagem.Text = "";
+        }
+
+        // Fecha a conexão com o servidor
+        private void FechaConexao(string Motivo)
+        {
+            // Mostra o motivo porque a conexão encerrou
+            txtLog.AppendText(Motivo + "\r\n");
+            // Habilita e desabilita os controles apropriados no formulario
+            txtServidorIP.Enabled = true;
+            txtUsuario.Enabled = true;
+            txtMensagem.Enabled = false;
+            btnEnviar.Enabled = false;
+            btnConectar.Text = "Conectado";
+
+            // Fecha os objetos
+            Conectado = false;
+            stwEnviador.Close();
+            strReceptor.Close();
+            tcpServidor.Close();
+        }
+
+        // O tratador de evento para a saida da aplicação
+        public void OnApplicationExit(object sender, EventArgs e)
+        {
+            if (Conectado == true)
+            {
+                // Fecha as conexões, streams, etc...
+                Conectado = false;
+                stwEnviador.Close();
+                strReceptor.Close();
+                tcpServidor.Close();
+            }
+        }
+    }
+}
